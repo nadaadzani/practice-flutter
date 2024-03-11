6a2aa3de6b811dfc7abe5ead2dc4c1c0e8ea3b75
adzaninada <adzaninada@gmail.com>
2024-03-11 10:53:51 +0700

    dart and flutter essentials

---
+++ essentials/dart.js
@@ -0,0 +1,86 @@
+// Dart is also known as statically typed language meaning that once we declare a variable and we give that variable a type, we cannot change that type later on.
+
+// Variables in Dart are declared using the var keyword. The type of the variable is inferred from the value assigned to it.
+// There is a variable for integers called int, a variable for floating-point numbers called double, a variable for strings called String, and a variable for booleans called bool.
+
+// Although you can't change the type of a variable using int, String, bool and so on. But you can use dynamic to declare a variable that can change its type (not recommended)
+
+// The console.log equivalent in Dart is print.
+
+
+
+// Functions in Dart are a bit different from JavaScript.
+// You have to declare a function that has type in front of it
+
+// For example:
+// void main() {
+//   print('Hello, World!');
+// }
+
+// The void keyword is used to indicate that the function does not return a value.
+
+// If you want to return a value, you have to declare the type of the value that the function will return.
+
+// For example:
+// String greet() {
+//   return 'Hello, World!';
+// }
+
+// To declare an array variable in Dart, we use the List keyword.
+// For example:
+// List<int> numbers = [1, 2, 3, 4, 5];
+
+// To manipulate data in the array, we can use the following methods:
+// numbers.add(6); // Add an item to the end of the list
+// numbers.remove(3); // Remove an item from the list
+// numbers.length; // Get the length of the list
+// numbers.isEmpty; // Check if the list is empty
+
+// ----------------------------
+
+// To declare a class in Dart, we use the class keyword.
+// similar to JavaScript, we can declare a constructor, properties, and methods inside a class.
+// For example:
+/*
+    class User {
+        String name = 'pozy';
+        int age = 20;
+
+        User(this.name, this.age);
+
+        void greet() {
+            print('Hello, my name is $name and I am $age years old');
+        }
+    }
+*/
+
+// To create an instance of the class, we need to put the type of User infront of the class, and invoke it afterwards.
+/* void main() {
+    User userTest = User();
+    print(userTest.name);
+    print(userTest.greet());
+} */
+
+// You can use constructors in class like this:
+/*
+    class User {
+        String name;
+        int age;
+
+        User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        void greet() {
+            print('Hello, my name is $name and I am $age years old');
+        }
+    }
+*/
+
+// And these constructors can be used on our main function by passing an argument onto it.
+/* void main() {
+    User userTest = User('pozy', 20);
+    print(userTest.name);
+    print(userTest.greet());
+} */
---
+++ essentials/widget.js
@@ -0,0 +1,11 @@
+// To build any applications in Flutter, we start with widgets – The building block of flutter applications. Widgets describe what their view should look like given their current configuration and state. It includes a text widget, row widget, column widget, container widget, and many more.
+
+// Widgets: Each element on a screen of the Flutter app is a widget. The view of the screen completely depends upon the choice and sequence of the widgets used to build the apps. And the structure of the code of an apps is a tree of widgets
+
+// It's like a component in React, but in Flutter, everything is a widget.
+
+// For example there's a widget for a button, a widget for a text input, a widget for a row of items, a widget for a column of items, a widget for a container with a border, a widget for a container with rounded corners, a widget for a container with a shadow, and so on.
+
+// On text widgets there are: style, textAlign, overflow, maxLines etc.
+// On button widgets there are: color, eleveation, disabledColor, enabled etc.
+
